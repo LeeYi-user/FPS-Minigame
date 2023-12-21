202713010acda73dd2530c6d21c56e5ecd25774f
LeeYi-user <khes60122@gmail.com>
2023-12-21 15:28:24 +0800

    finish gameplay

--- Assets/Scripts/PlaySceneManager.cs
+++ Assets/Scripts/PlaySceneManager.cs
@@ -21,6 +21,23 @@ public class PlaySceneManager : MonoBehaviour
     [SerializeField] private Slider damageSlider;
     [SerializeField] private Slider fireRateSlider;
 
+
+    public static int healthPrice = 100;
+    public static int speedPrice = 100;
+    public static int jumpPrice = 100;
+
+    public static int ammoPrice = 100;
+    public static int damagePrice = 100;
+    public static int fireRatePrice = 100;
+
+    [SerializeField] private TextMeshProUGUI healthPriceText;
+    [SerializeField] private TextMeshProUGUI speedPriceText;
+    [SerializeField] private TextMeshProUGUI jumpPriceText;
+
+    [SerializeField] private TextMeshProUGUI ammoPriceText;
+    [SerializeField] private TextMeshProUGUI damagePriceText;
+    [SerializeField] private TextMeshProUGUI fireRatePriceText;
+
     public static float health = 60f;
     public static float speed = 4.2f;
     public static float jump = 7.2f;
@@ -42,21 +59,10 @@ public class PlaySceneManager : MonoBehaviour
     {
         moneyText.text = "$ " + money.ToString();
         wavesText.text = "Waves: " + EnemySpawner.waves.ToString();
-        health = healthSlider.value * 300f;
-        speed = speedSlider.value * 21f;
-        jump = jumpSlider.value * 36f;
-        ammo = (int)(ammoSlider.value * 30);
-        damage = damageSlider.value * 50f;
-        fireRate = fireRateSlider.value * 10f;
 
         if (Input.GetKeyDown(KeyCode.P))
         {
-            healthSlider.interactable = true;
-            speedSlider.interactable = true;
-            jumpSlider.interactable = true;
-            ammoSlider.interactable = true;
-            damageSlider.interactable = true;
-            fireRateSlider.interactable = true;
+            money += 100000;
         }
     }
 
@@ -67,6 +73,156 @@ public class PlaySceneManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         gameOver = true;
+    }
+
+    public void UpgradeHealth()
+    {
+        if (healthSlider.value >= 10f)
+        {
+            return;
+        }
+
+        if (money >= healthPrice)
+        {
+            healthSlider.value += 1f;
+            health = healthSlider.value * 30f;
+            money -= healthPrice;
+            healthPrice *= 2;
+
+            if (healthSlider.value < 10f)
+            {
+                healthPriceText.text = healthPrice.ToString() + " $";
+            }
+            else
+            {
+                healthPriceText.text = "MAX";
+            }
+        }
+    }
+
+    public void UpgradeSpeed()
+    {
+        if (speedSlider.value >= 10f)
+        {
+            return;
+        }
+
+        if (money >= speedPrice)
+        {
+            speedSlider.value += 1f;
+            speed = speedSlider.value * 2.1f;
+            money -= speedPrice;
+            speedPrice *= 2;
+
+            if (speedSlider.value < 10f)
+            {
+                speedPriceText.text = speedPrice.ToString() + " $";
+            }
+            else
+            {
+                speedPriceText.text = "MAX";
+            }
+        }
+    }
+
+    public void UpgradeJump()
+    {
+        if (jumpSlider.value >= 10f)
+        {
+            return;
+        }
+
+        if (money >= jumpPrice)
+        {
+            jumpSlider.value += 1f;
+            jump = jumpSlider.value * 3.6f;
+            money -= jumpPrice;
+            jumpPrice *= 2;
+
+            if (jumpSlider.value < 10f)
+            {
+                jumpPriceText.text = jumpPrice.ToString() + " $";
+            }
+            else
+            {
+                jumpPriceText.text = "MAX";
+            }
+        }
+    }
+
+    public void UpgradeAmmo()
+    {
+        if (ammoSlider.value >= 10f)
+        {
+            return;
+        }
+
+        if (money >= ammoPrice)
+        {
+            ammoSlider.value += 1f;
+            ammo = (int)(ammoSlider.value * 3);
+            money -= ammoPrice;
+            ammoPrice *= 2;
+
+            if (ammoSlider.value < 10f)
+            {
+                ammoPriceText.text = ammoPrice.ToString() + " $";
+            }
+            else
+            {
+                ammoPriceText.text = "MAX";
+            }
+        }
+    }
+
+    public void UpgradeDamage()
+    {
+        if (damageSlider.value >= 10f)
+        {
+            return;
+        }
+
+        if (money >= damagePrice)
+        {
+            damageSlider.value += 1f;
+            damage = damageSlider.value * 5f;
+            money -= damagePrice;
+            damagePrice *= 2;
+
+            if (damageSlider.value < 10f)
+            {
+                damagePriceText.text = damagePrice.ToString() + " $";
+            }
+            else
+            {
+                damagePriceText.text = "MAX";
+            }
+        }
+    }
+
+    public void UpgradeFireRate()
+    {
+        if (fireRateSlider.value >= 10f)
+        {
+            return;
+        }
+
+        if (money >= fireRatePrice)
+        {
+            fireRateSlider.value += 1f;
+            fireRate = fireRateSlider.value;
+            money -= fireRatePrice;
+            fireRatePrice *= 2;
+
+            if (fireRateSlider.value < 10f)
+            {
+                fireRatePriceText.text = fireRatePrice.ToString() + " $";
+            }
+            else
+            {
+                fireRatePriceText.text = "MAX";
+            }
+        }
     }
 
     public void BackFromUpgrade()
